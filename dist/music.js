646b670ada5d13eab12e44658561194dc89ec876
Yesord <2691004662@qq.com>
2023-11-25 00:34:06 +0800

    Site updated: 2023-11-25 00:34:05

--- dist/music.js
+++ dist/music.js
@@ -15,15 +15,36 @@ const ap = new APlayer({
             name: 'One Last Kiss',
             artist: '宇多田ヒカル',
             url:  'https://cccimg.com/view.php/1edf91bfbc64b30508dde1bbbc116311.mp3',
-            cover: 'cover1.jpg',
+            cover: 'https://s2.loli.net/2023/11/25/Sd9Aqs6O8uhYCj2.jpg',
             theme: '#ebd0c2' ,//切换到此音频时的主题色，比上面的 theme 优先级高
         },
         {
             name: 'Lemon',
             artist: '米津玄师',
             url: 'https://cccimg.com/view.php/ac10dd5f17a986917a6f83112652350f.mp3',
-            cover: 'cover2.jpg',
-            theme: '#ebd0c2' ,//切换到此音频时的主题色，比上面的 theme 优先级高
-        }
+            cover: 'https://s2.loli.net/2023/11/25/v9dUAaYXLRTh2S5.jpg',
+            theme: '#FF0000' ,//切换到此音频时的主题色，比上面的 theme 优先级高
+        },
+        {
+            name: '丑八怪',
+            artist: '薛之谦',
+            url: 'https://cccimg.com/view.php/9c2115b1eab784f4cf91f5b31cf60a3e.mp3',
+            cover: 'https://s2.loli.net/2023/11/25/aTqCXzuVSwF3efG.png',
+            theme: '#FF0000' ,//切换到此音频时的主题色，比上面的 theme 优先级高
+        },
+        {
+            name: '天外来物',
+            artist: '薛之谦',
+            url: 'https://cccimg.com/view.php/3f8fe113124e8a2bf463451aadb5883e.mp3',
+            cover: 'https://s2.loli.net/2023/11/25/Z8aFpnd37fTUmoj.jpg',
+            theme: '#FF0000' ,//切换到此音频时的主题色，比上面的 theme 优先级高
+        },
+        {
+            name: '我好像在哪见过你',
+            artist: '薛之谦',
+            url: 'https://cccimg.com/view.php/ac32419bf476bc269ecf635c61931aae.mp3',
+            cover: 'https://s2.loli.net/2023/11/25/Uv8YPuwaZzcpl71.jpg',
+            theme: '#FF0000' ,//切换到此音频时的主题色，比上面的 theme 优先级高
+        },
     ]
 });
