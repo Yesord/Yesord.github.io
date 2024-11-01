a1ef61bd66da00581d45bc98e199fd162ee12c66
Mikazuki Tomoko <142188839+Yesord@users.noreply.github.com>
2024-11-01 09:41:55 +0800

    Site updated: 2024-11-01 09:41:54

--- page/live2d-widget/waifu.css
+++ page/live2d-widget/waifu.css
@@ -1,11 +1,11 @@
 <!doctype html> <!-- 文档类型 -->
 <html lang="en"> <!-- 语言 -->
-    <head><!-- hexo injector head_begin start --><meta name="description" content="010101110110111001"></meta><meta name="keywords" content="tech, Blog"></meta><meta name="author" content="Yesord"></meta><!-- hexo injector head_begin end --><meta charset="utf-8"> <!-- 字符集 -->
+    <head><!-- hexo injector head_begin start --><meta name="description" content="010101110110111001"></meta><meta name="keywords" content="MCU, Blog"></meta><meta name="author" content="PIKO"></meta><!-- hexo injector head_begin end --><meta charset="utf-8"> <!-- 字符集 -->
         <meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- IE浏览器兼容 -->
         <meta name="viewport" content="width=device-width,initial-scale=1"> <!-- 移动端适配 -->
         <link rel="icon" href="/favicon.ico"><!-- 网站的初始图标 -->
         <script type="module" crossorigin src="/static/js/14c27e7f.js"></script> <!-- 依赖 -->
-        <link rel="stylesheet" href="/static/css/d5346b99.css"><!-- hexo injector head_end start --><script rel="prefetch" async src="https://busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script><script rel="preload" src="https://unpkg.com/blueimp-md5@^2.19.0/js/md5.min.js"></script><script rel="preload" src="https://unpkg.com/lodash@^4.17.21/lodash.min.js"></script><link rel="stylesheet" href="https://fonts.loli.net/css?family=Rubik" /><script rel="preload" src="https://unpkg.com/valine@^1.5.1/dist/Valine.min.js"></script><script rel="preload" src="https://unpkg.com/leancloud-storage@^4.15.0/dist/av-min.js"></script><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css"><!-- hexo injector head_end end --><meta name="generator" content="Hexo 6.3.0"></head>  <!-- 主题css -->
+        <link rel="stylesheet" href="/static/css/d5346b99.css"><!-- hexo injector head_end start --><script rel="prefetch" async src="https://busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script><script rel="preload" src="https://unpkg.com/blueimp-md5@^2.19.0/js/md5.min.js"></script><script rel="preload" src="https://unpkg.com/lodash@^4.17.21/lodash.min.js"></script><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Rubik"/><script rel="preload" src="https://unpkg.com/valine@^1.5.1/dist/Valine.min.js"></script><script rel="preload" src="https://unpkg.com/leancloud-storage@^4.15.0/dist/av-min.js"></script><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css"><!-- hexo injector head_end end --><meta name="generator" content="Hexo 6.3.0"></head>  <!-- 主题css -->
         
         <body id="body-container">
             <link rel="stylesheet" href="/dist/APlayer.locate.css"> <!-- APlayer的位置css -->
