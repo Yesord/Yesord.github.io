5daaa425b99dfd74e19a1da168bf184ced9c131f
Mikazuki Tomoko <142188839+Yesord@users.noreply.github.com>
2025-03-02 23:56:27 +0800

    Site updated: 2025-03-02 23:56:26

--- static/js/preload.js
+++ static/js/preload.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Show loading animation
+    const loadingBackground = document.createElement('div');
+    loadingBackground.className = 'loading-background';
+    loadingBackground.style.position = 'fixed';
+    loadingBackground.style.top = '0';
+    loadingBackground.style.left = '0';
+    loadingBackground.style.width = '100%';
+    loadingBackground.style.height = '100%';
+    loadingBackground.style.backgroundColor = '#e7e7e2';
+    loadingBackground.style.zIndex = '9998'; // Ensure the loading background is just below the animation
+    document.body.appendChild(loadingBackground);
+
     const loadingAnimation = document.createElement('div');
     loadingAnimation.className = 'polygon-loader';
     loadingAnimation.innerHTML = `
@@ -18,14 +29,15 @@ document.addEventListener("DOMContentLoaded", function() {
         return new Promise((resolve) => {
             setTimeout(() => {
                 resolve();
-            }, 500); // Simulate a 3-second loading time
+            }, 1000); // Simulate a 3-second loading time
         });
     }
 
     // Load resources
     loadResources().then(() => {
-        // Hide loading animation
+        // Hide loading animation and background
         document.body.removeChild(loadingAnimation);
+        document.body.removeChild(loadingBackground);
 
         // Show the main content
         const mainContent = document.getElementById('app');
@@ -39,7 +51,7 @@ document.addEventListener("DOMContentLoaded", function() {
             takina.initialState = {
                 ...takina.initialState,
                 i: 0.01,
-                d: 1,
+                d: 0.98,
             };
             SakanaWidget.registerCharacter('takina-slow', takina);
             new SakanaWidget({ character: 'takina-slow' }).mount('#sakana-widget');
@@ -56,6 +68,18 @@ document.addEventListener("DOMContentLoaded", function() {
         live2dScript.src = 'https://fastly.jsdelivr.net/gh/Yesord/live2d-widget@latest/autoload.js';
         document.body.appendChild(live2dScript);
 
-        
+        // Load additional scripts
+        const scripts = [
+            '/dist/APlayer.min.js',
+            '/dist/music.js',
+            '/static/js/ripple.js',
+            '/static/js/mouse.js'
+        ];
+
+        scripts.forEach(src => {
+            const script = document.createElement('script');
+            script.src = src;
+            document.body.appendChild(script);
+        });
     });
 });
